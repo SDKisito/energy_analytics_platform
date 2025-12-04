f647757c895b7f09927edc1425cac98e6a4f8967
Saliou DIEDHIOU <133891956+SDKisito@users.noreply.github.com>
2025-12-04 11:17:34 +0100

    Update SiteHeatmap.tsx

--- src/pages/energy-overview-dashboard/components/SiteHeatmap.tsx
+++ src/pages/energy-overview-dashboard/components/SiteHeatmap.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import { LineChart, Line, ResponsiveContainer } from 'recharts';
 import Icon from '../../../components/AppIcon';
 import { SiteHeatmapData } from '../types';
